5e04147951bdc9df19f6d3ce00de1f345c29181e
OGBA <ogbarules@rocketmail.com>
2016-09-28 20:04:26 -0400

    bfs

--- ass1/src/ass1/strategies/BFSStrategy.java
+++ ass1/src/ass1/strategies/BFSStrategy.java
@@ -1,15 +1,14 @@
 
 package ass1.strategies;
-
 import ass1.Robot;
 import ass1.RobotAction;
 import ass1.Solution;
 
 public class BFSStrategy implements SearchStrategyInterface {
-    private Solution solution;
+    private Solution solution;//
     
     public BFSStrategy() {
-    	solution = new Solution();	
+    	solution = new Solution();	//
     }
     
 	public void search(Robot robot) {
@@ -17,7 +16,7 @@ public class BFSStrategy implements SearchStrategyInterface {
 	}
 
 	public Solution getSolution() {
-		return solution;
+		return solution;//
 	}
 	
 }
